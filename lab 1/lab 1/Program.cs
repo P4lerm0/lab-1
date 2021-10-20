b4b9d490244c210887cd3d015d38e5b60a0651e8
P4lerm0 <90239535+P4lerm0@users.noreply.github.com>
2021-10-20 10:10:01 +0300

    Add files via upload

---
+++ lab 1/lab 1/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Title = "InformationBoard";
+Console.ForegroundColor = ConsoleColor.Green;
+Console.BackgroundColor = ConsoleColor.Black;
+Console.WriteLine("SystemInformation");
+
+
+Console.WriteLine(Environment.MachineName);
+Console.WriteLine(Environment.OSVersion);
+Console.WriteLine(Environment.UserDomainName);
+Console.WriteLine(Environment.UserName);
+Console.WriteLine(Environment.Version);
+Console.WriteLine(Environment.CurrentDirectory);
+Console.WriteLine(Environment.CurrentManagedThreadId);
+Console.WriteLine(Environment.ProcessId);
+Console.WriteLine(Environment.ProcessorCount);
+Console.WriteLine(Environment.Is64BitProcess);
+Console.WriteLine(Environment.Is64BitOperatingSystem);
+
+Console.WriteLine(Console.ReadLine());
+
+Console.ReadKey();
+Console.Clear();
